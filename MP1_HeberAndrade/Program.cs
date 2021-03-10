3ba0fcaab36ab460d17c54dcc156336fc392abfd
Heber Andrade <hebera75@gmail.com>
2021-03-10 15:59:42 +0100

    Presentation
    Presentation

--- MP1_HeberAndrade/Program.cs
+++ MP1_HeberAndrade/Program.cs
@@ -6,7 +6,40 @@ namespace MP1_HeberAndrade
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string productName;
+
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine($">.........................................<\n");
+            Console.WriteLine($"       Welcomen to IDESIGNER.SE\n");
+
+            Console.WriteLine($">.........................................<\n");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"              INVENTORY        \n");
+            Console.WriteLine($">.........................................<\n");
+
+            Console.WriteLine($"What product are you looking for? :\n\nPRESS Key ENTER\n\nOR 'q' TO QUIT.\n");
+            Console.ResetColor();
+
+            Console.Write($" Write the name of the product looking for: ? : ");
+            productName = Console.ReadLine();
+
+            while (true)
+            {
+
+                string insert = Console.ReadLine();
+
+                if (insert == "q")
+                {
+                    Console.WriteLine("\n\nBye, Thanks for your visit!\n\n");
+                    break;
+                }
+
+                if (insert != "q")
+                {
+
+                }
+            }
         }
     }
 }
